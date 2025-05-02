d75000d0aea52b73d808db0382b2bb7a71ba0f55
Sasha <sasha.vessa@gmail.com>
2025-05-01 21:23:02 -0300

    first commit

---
+++ CadastrosHEHEHEH/src/main/java/aka/Sasha/CadastrosHEHEHEH/CadastrosHehehehApplication.java
@@ -0,0 +1,13 @@
+package aka.Sasha.CadastrosHEHEHEH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosHehehehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosHehehehApplication.class, args);
+	}
+
+}
---
+++ CadastrosHEHEHEH/src/test/java/aka/Sasha/CadastrosHEHEHEH/CadastrosHehehehApplicationTests.java
@@ -0,0 +1,13 @@
+package aka.Sasha.CadastrosHEHEHEH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosHehehehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
