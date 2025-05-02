84e5bd1b07a36dcb64d9b3f83b31d2333a458f13
Sasha <sasha.vessa@gmail.com>
2025-05-02 16:58:09 -0300

    controller criada

---
+++ CadastrosHEHEHEH/src/main/java/aka/Sasha/CadastrosHEHEHEH/Controller.java
@@ -0,0 +1,18 @@
+package aka.Sasha.CadastrosHEHEHEH;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas") // colcar as "abas" paginas de navegação
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+
+    }
+
+
+}
