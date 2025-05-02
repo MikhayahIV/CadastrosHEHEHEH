ff3cca41ebad9266e7406fad3c8ec22b3250b56a
Sasha <sasha.vessa@gmail.com>
2025-05-02 18:00:06 -0300

    cria uma entidade no banco de dados apartir do UsuariosModel (Dia 2)

---
+++ CadastrosHEHEHEH/src/main/java/aka/Sasha/CadastrosHEHEHEH/UsuariosModel.java
@@ -0,0 +1,63 @@
+package aka.Sasha.CadastrosHEHEHEH;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // ele transforma uma classe em uma entidade do banco de dados
+@Table(name = "tb_cadastro")
+public class UsuariosModel {
+
+
+    @Id
+    @GeneratedValue
+    long id;
+    String nome;
+    String email;
+    String NumeroCelular;
+    int idade;
+
+    public UsuariosModel() {
+
+    }
+
+    public UsuariosModel(String nome, String email, String NumeroCelular, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.NumeroCelular = NumeroCelular;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumeroCelular() {
+        return NumeroCelular;
+    }
+
+    public void setNumeroCelular(String numeroCelular) {
+        this.NumeroCelular = numeroCelular;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
