aa8e10ad6a4c2a0251a82108aacdf7021af915d6
Sasha <sasha.vessa@gmail.com>
2025-05-03 19:14:23 -0300

    Olha o Lombok aí

--- CadastrosHEHEHEH/src/main/java/aka/Sasha/CadastrosHEHEHEH/Atividades/AtividadesModel.java
+++ CadastrosHEHEHEH/src/main/java/aka/Sasha/CadastrosHEHEHEH/Atividades/AtividadesModel.java
@@ -2,11 +2,17 @@ package aka.Sasha.CadastrosHEHEHEH.Atividades;
 
 import aka.Sasha.CadastrosHEHEHEH.Usuarios.UsuariosModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name="tb_Atividades")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AtividadesModel {
 
     @Id
--- CadastrosHEHEHEH/src/main/java/aka/Sasha/CadastrosHEHEHEH/Usuarios/UsuariosModel.java
+++ CadastrosHEHEHEH/src/main/java/aka/Sasha/CadastrosHEHEHEH/Usuarios/UsuariosModel.java
@@ -2,11 +2,17 @@ package aka.Sasha.CadastrosHEHEHEH.Usuarios;
 
 import aka.Sasha.CadastrosHEHEHEH.Atividades.AtividadesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity //
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuariosModel {
 
 
@@ -27,46 +33,5 @@ public class UsuariosModel {
     @JoinColumn(name = "atividade_id") //chave estrangeira ou Foreing Key(fk)
     private AtividadesModel atividade;
 
-    public UsuariosModel() {
 
-    }
-
-    public UsuariosModel(String nome, String email, String NumeroCelular, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.NumeroCelular = NumeroCelular;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNumeroCelular() {
-        return NumeroCelular;
-    }
-
-    public void setNumeroCelular(String numeroCelular) {
-        this.NumeroCelular = numeroCelular;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
